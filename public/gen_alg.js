eb1f284315ae63799b102b616fc76820bd0cdad2
gustavoresque <gustavo.cbcc@gmail.com>
2025-02-19 22:29:00 -0300

    ajustes no gráfico de linhas e no AG

--- public/gen_alg.js
+++ public/gen_alg.js
@@ -1,34 +1,27 @@
-//Código para enviar dados via WebSocket
-const WebSocket = require('ws');
-const ws = new WebSocket('ws://localhost:8080');
 
-ws.on('open', () => {
-    console.log("Connected to WebSocket server");
-});
-
-const numNodes = 10;
 const populationSize = 100;
 const mutationRate = 0.05;
 const survivalRate = 0.5;
-const newIndividual = 100;
-const generations = 1000;
+
 
 
 class GA{
 
-    constructor(individualConstructor){
+    constructor(individualConstructor, numNodes){
         this.newIndividual = individualConstructor
         this.generatePopulation = GA.generatePopulation.simples;
         this.fitness = GA.fitness.std;
         this.crossover = GA.crossover.simples;
         this.mutate = GA.mutate.simples;
         this.selection = GA.selection.simples;
-
+        this.generation = 0;
+        this.numNodes = numNodes;
     }
 
     init(){
-        this.population = this.generatePopulation(this.newIndividual);
+        this.population = this.generatePopulation(this.newIndividual, this.numNodes);
         this.fitness(this.population);
+        this.generation = 1;
     }
 
     nextGeneration(){
@@ -37,6 +30,7 @@ class GA{
         this.fitness(newPopulation);
         this.oldPopulation = this.population;
         this.population = this.selection(this.population, newPopulation);
+        this.generation++;
     }
 }
 GA.fitness={};
@@ -81,7 +75,7 @@ GA.selection.simples = (oldPopulation, newPopulation) => {
 }
 
 GA.generatePopulation ={};
-GA.generatePopulation.simples = (newIndividual) => {
+GA.generatePopulation.simples = (newIndividual, numNodes) => {
     let population = [];
     for (let i = 0; i < populationSize; i++) {
         let individual = newIndividual(Array.from({ length: numNodes }, 
@@ -95,71 +89,6 @@ GA.generatePopulation.simples = (newIndividual) => {
 
 
 
-function generateGraph(numNodes) {
-    let graph = Array.from({ length: numNodes }, () => Array(numNodes).fill(0));
-    for (let i = 0; i < numNodes; i++) {
-        for (let j = i + 1; j < numNodes; j++) {
-            graph[i][j] = graph[j][i] = Math.random() > 0.5 ? 1 : 0;
-        }
-    }
-    return graph;
-}
 
-const graph = generateGraph(numNodes);
-
-function randomIndividual() {
-    return Array.from({ length: numNodes }, () => Math.random() > 0.5 ? 1 : 0);
-}
-
-function fitness(individual) {
-    let count = individual.reduce((sum, bit) => sum + bit, 0);
-    for (let i = 0; i < numNodes; i++) {
-        for (let j = i + 1; j < numNodes; j++) {
-            if (individual[i] && individual[j] && !graph[i][j]) {
-                return 0;
-            }
-        }
-    }
-    return count;
-}
-
-function mutate(individual) {
-    return individual.map(bit => Math.random() < mutationRate ? 1 - bit : bit);
-}
-
-function crossover(parent1, parent2) {
-    const midpoint = Math.floor(Math.random() * parent1.length);
-    return parent1.slice(0, midpoint).concat(parent2.slice(midpoint));
-}
-
-function evolve() {
-    let population = Array.from({ length: populationSize }, randomIndividual);
-    for (let gen = 0; gen < generations; gen++) {
-        population.sort((a, b) => fitness(b) - fitness(a));
-        console.log(`Generation ${gen}: Best fitness = ${fitness(population[0])}`);
-
-        // Envia o melhor indivíduo para o WebSocket
-        ws.send(JSON.stringify({
-            generation: gen,
-            fitness: fitness(population[0]),
-            genes: population[0].genes
-        }));
-
-        if (fitness(population[0]) === numNodes) {
-            console.log(`Maximum clique found in generation ${gen}:, population[0]`);
-            return;
-        }
-        let newPopulation = population.slice(0, populationSize / 2);
-        while (newPopulation.length < populationSize) {
-            let [p1, p2] = [newPopulation[Math.floor(Math.random() * newPopulation.length)],
-                            newPopulation[Math.floor(Math.random() * newPopulation.length)]];
-            let child = mutate(crossover(p1, p2));
-            newPopulation.push(child);
-        }
-        population = newPopulation;
-    }
-}
-
-evolve();
 
 if(typeof module !== "undefined") module.exports = {GA};
--- public/server.js
+++ public/server.js
@@ -1,14 +1,61 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 8080 });
+const server = new WebSocket.Server({ port: 3214 });
+const clients = [];
+
+console.log("WebSocket server running on ws://localhost:3214");
 
 server.on('connection', ws => {
     console.log("Client connected");
     
+    clients.push(ws);
     ws.on('message', message => {
         console.log(`Received: ${message}`);
     });
 
-    ws.send("Connected to WebSocket server");
+    ws.send(JSON.stringify({act:"log", data:"Connected!"}));
 });
 
-console.log("WebSocket server running on ws://localhost:8080");
+
+
+const fs = require("fs");
+const {Graph, CliqueBuilder, CliqueSolver, CliqueMask} = require("./graph.js");
+const {GA} = require("./gen_alg.js");
+
+// let dbpath = "../exemplosGrafos/grafoK5.txt";
+// let dbpath = "../exemplosGrafos/homer.col.txt";
+let dbpath = "../exemplosGrafos/queen5_5.col.txt";
+// let dbpath = "../exemplosGrafos/clique34.txt";
+// let dbpath = "../exemplosGrafos/clique34.txt";
+
+
+let txt = fs.readFileSync(dbpath, {encoding:"utf-8"});
+
+let graph = new Graph();
+graph.importFromText(txt);
+graph.calcMatAdjs();
+
+let ga = new GA(CliqueMask.getConstructor(graph), graph.nodes.length);
+
+
+ga.init();
+
+setInterval(()=>{
+    ga.nextGeneration();
+    
+    for (const c of clients) {
+        c.send(JSON.stringify({act:"data", data:{
+            bestFitness: ga.population[0].fitness, 
+            generation: ga.generation
+        }}));
+    }
+
+    
+    console.log(`Best Fitness: ${ga.population[0].fitness}`);
+    console.log(`Best age: ${ga.population[0].age}`);
+    console.log(ga.population[0].nodeMask);
+    console.log(`generation: ${ga.generation}`);
+},900);
+
+
+
+
